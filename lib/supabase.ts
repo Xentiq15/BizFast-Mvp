7ef4af6ea58a9aa88c45bb8fa705f67c137348b6
Xentiq15 <oliver.e.beijer@gmail.com>
2025-08-06 11:36:27 +0000

    Ditt meddelande här

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.https://pdttzezmqrgaukopigoq.supabase.co!;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdHR6ZXptcXJnYXVrb3BpZ29xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0NzQ4MTgsImV4cCI6MjA3MDA1MDgxOH0.QgY19dE_EZ41UHC_7Ds7h6dxEUuO_I0JjB8HRFvy5XQ!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
